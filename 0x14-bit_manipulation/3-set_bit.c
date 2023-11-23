7e97e7bebfb0ca3fee2612f88886c46ab9ae84e9
Marwa-Elhag <marwaalhaj89@gmail.com>
2023-11-23 15:16:45 -0800

    MARWA

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - value of bit
+ * @n: pointer
+ * @index: index
+ * Return: success
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n |= (1UL << index);
+	return (1);
+}
+
